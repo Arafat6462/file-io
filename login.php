16966c57ae74aa75f7caf3fd3bc60485b21aec85
Arafat <arafat6462@gmail.com>
2021-06-18 19:12:57 +0600

    added file I/O on ret-form

---
+++ login.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Log-in</title>
+</head>
+<body>
+	<h1>Log-in Form</h1>
+ 
+	<?php
+
+		fopen("date.txt", "r");
+
+		$data = file_get_contents("data.txt");
+		$dataOK = json_decode($data);
+		// echo "hello";
+		// echo $dtlsOK->Religion;
+		// echo "hi";
+
+		// echo $data."<br><br>";
+		// echo "new : ".$dataOK;
+
+		$user = $dataOK->Username;
+		$pass = $dataOK->Password;
+
+		if("a" == $_POST['Username'] && "b" == $_POST['Password'])
+		{
+			header('Location: welcome.html');
+			//exit;
+		}
+		else
+		{
+			$failed = "Log-in failed";
+		}
+
+		// foreach ($dtlsOK as $ok  )
+		// {
+		// 	echo "$ok->Religion";
+		// 	echo "$ok->Firstname";
+		// }
+
+
+
+ 	 	// $filepointer = fopen("data.txt", "r");
+
+ 	 	// if ($filepointer) 
+ 	 	// {
+ 	 	// 	 $content = fread($filepointer, filesize("data.txt"));
+ 	 	// 	 echo $content;
+ 	 	// 	 fclose($filepointer);
+ 	 	// }
+	?>
+	
+	<form action="<?php echo htmlspecialchars(($_SERVER['PHP_SELF'])); ?>" method = "POST">
+ 	 
+ 		 
+			<label for="Username">Username:</label>
+			<input type="text" id="Username" name="Username" required><br>
+
+			<label for="Password">Password:</label>
+			<input type="Password" id="Password" name="Password" required><br>
+
+ 		<br>
+		<input type="submit" value="Log-in">
+		<span style="color: red"><?php echo $failed; ?></span>
+
+		
+	</form>
+</body>
+</html>
