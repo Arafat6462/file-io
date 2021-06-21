7d876c65f59a5ff50b01308cab84a66a494870af
Arafat <arafat6462@gmail.com>
2021-06-21 16:17:50 +0600

    Fixed json parse and  login validation

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Form Submission</title>
+	<title>File I/O</title>
 </head>
 <body>
 
-	<h1>Form Submission</h1>
+	<h1>File I/O</h1>
 
 	<?php
 		// $arr = array("name"=>"arafat","gender"=>"male","age"=>25);
@@ -19,16 +19,16 @@
 
 	if ($_SERVER['REQUEST_METHOD'] === "POST")
 	{
-		$firstName = $_POST['Firstname'];
-		write(basic_validation($firstName));
+		// $firstName = $_POST['Firstname'];
+		// write(basic_validation($firstName));
 
 		$array = array( "Firstname"=>basic_validation($_POST['Firstname']),"Lastname"=>basic_validation($_POST['Lastname']),
-						"Gender"=>basic_validation($_POST['Gender']),"DOB"=>basic_validation($_POST['DOB']),
-						"Religion"=>basic_validation($_POST['Religion']),"presentaddress"=>basic_validation($_POST['presentaddress']),
-						"Permanentaddress"=>basic_validation($_POST['Permanentaddress']),"phone"=>basic_validation($_POST['phone']),
-						"Email"=>basic_validation($_POST['Email']),"Lienk"=>basic_validation($_POST['Lienk']),
-						"Username"=>basic_validation($_POST['Username']),"Password"=>basic_validation($_POST['Password']),
-					);
+			"Gender"=>basic_validation($_POST['Gender']),"DOB"=>basic_validation($_POST['DOB']),
+			"Religion"=>basic_validation($_POST['Religion']),"presentaddress"=>basic_validation($_POST['presentaddress']),
+			"Permanentaddress"=>basic_validation($_POST['Permanentaddress']),"phone"=>basic_validation($_POST['phone']),
+			"Email"=>basic_validation($_POST['Email']),"Lienk"=>basic_validation($_POST['Lienk']),
+			"Username"=>basic_validation($_POST['Username']),"Password"=>basic_validation($_POST['Password']),
+		);
 
 		
 
@@ -37,91 +37,91 @@
 	}
 
 	// validate input
- 	function basic_validation($data)
- 	{
- 		$data = trim($data);
- 		$data = htmlspecialchars($data);
- 		$data = stripcslashes($data);
- 		return $data;
- 	}
+	function basic_validation($data)
+	{
+		$data = trim($data);
+		$data = htmlspecialchars($data);
+		$data = stripcslashes($data);
+		return $data;
+	}
 
- 	// append to data.txt
- 	function write($content)
- 	{
- 		$filePointer = fopen("data.txt", "a");	
- 		fwrite($filePointer, $content."\n");
- 		fclose($filePointer);
-  	}
+ 	// write in data.txt
+	function write($content)
+	{
+		$filePointer = fopen("data.txt", "w");	
+		fwrite($filePointer, $content."\n");
+		fclose($filePointer);
+	}
 
 
 
 	?>
- 
+
 
 	<form action="<?php echo htmlspecialchars(($_SERVER['PHP_SELF'])); ?>" method = "POST">
 
 		<fieldset>
 			<legend>Basic Information:</legend>
 
-		<label for="fname">First Name:</label>
-		<input type="text" id="fname" name="Firstname" required><br>
+			<label for="fname">First Name:</label>
+			<input type="text" id="fname" name="Firstname" required><br>
 
-		<label for="lname">Last name: </label>
-		<input type="text" id="lname" name="Lastname" required><br>
+			<label for="lname">Last name: </label>
+			<input type="text" id="lname" name="Lastname" required><br>
 
 
 			Select Gender:<br>
-		 	<input type="radio" id="Male" name="Gender" value="Male" required>
-		 	<label for="Male">Male</label> <br>
+			<input type="radio" id="Male" name="Gender" value="Male" required>
+			<label for="Male">Male</label> <br>
 
-		 	<input type="radio" id="Female" name="Gender" value="Female">
-		 	<label for="Female">Female</label> <br>
+			<input type="radio" id="Female" name="Gender" value="Female">
+			<label for="Female">Female</label> <br>
 
-		 	<input type="radio" id="Other" name="Gender" value="Other">
-		 	<label for="Other">Other</label><br>
+			<input type="radio" id="Other" name="Gender" value="Other">
+			<label for="Other">Other</label><br>
 
 
-		 <label for="DOB">DOB:</label>
-		 <input type="date" id="DOB" name="DOB" required><br>
+			<label for="DOB">DOB:</label>
+			<input type="date" id="DOB" name="DOB" required><br>
 
-		 Religion:<br>
+			Religion:<br>
 			<select name="Religion" required > 
 				<option value="" name="" ></option> 
 				<option value="islam" name="Religion" >islam</option> 
 				<option value="hindu" name="Religion" >hindu</option> 
 				<option value="christian" name="Religion" >christian</option> 
 			</select>
-			</fieldset>
-			<br><br><br>
+		</fieldset>
+		<br><br><br>
 
 
 
 
 		<fieldset>
-		<legend>Contact Information:</legend>
- 
+			<legend>Contact Information:</legend>
 
-		<label for="presentaddress">presentaddress:</label>
-		<textarea id="presentaddress" name="presentaddress" rows="2" cols="20"></textarea><br>
 
-		<label for="Permanentaddress">Permanentaddress:</label>
-		<textarea id="Permanentaddress" name="Permanentaddress" rows="2" cols="20"></textarea><br>
+			<label for="presentaddress">presentaddress:</label>
+			<textarea id="presentaddress" name="presentaddress" rows="2" cols="20"></textarea><br>
 
-		
-	    <label for="phone">phone: </label>
-		<input type="tel" id="phone" name="phone"><br>
+			<label for="Permanentaddress">Permanentaddress:</label>
+			<textarea id="Permanentaddress" name="Permanentaddress" rows="2" cols="20"></textarea><br>
 
-		<label for="Email">Email: </label>
-		<input type="Email" id="Email" name="Email" required><br>
 
-		<label for="Lienk">Personal Website Lienk : </label>
-		 <input type="url" id="Lienk" name="Lienk"><br>
- 
-		
+			<label for="phone">phone: </label>
+			<input type="tel" id="phone" name="phone"><br>
+
+			<label for="Email">Email: </label>
+			<input type="Email" id="Email" name="Email" required><br>
+
+			<label for="Lienk">Personal Website Lienk : </label>
+			<input type="url" id="Lienk" name="Lienk"><br>
+
+
 		</fieldset>
 
 
-					<br><br><br>
+		<br><br><br>
 
 
 		<fieldset>
@@ -138,6 +138,6 @@
 		<input type="submit" value="submit">
 
 
- 	</form>
+	</form>
 </body>
 </html>
--- login.php
+++ login.php
@@ -7,32 +7,54 @@
 </head>
 <body>
 	<h1>Log-in Form</h1>
- 
+	
 	<?php
 
-		fopen("date.txt", "r");
+	if ($_SERVER['REQUEST_METHOD'] === "POST")
+	{
+
+		//fopen("date.txt", "r");
+
 
 		$data = file_get_contents("data.txt");
 		$dataOK = json_decode($data);
+		$failed = "";
+
+
 		// echo "hello";
 		// echo $dtlsOK->Religion;
 		// echo "hi";
 
-		// echo $data."<br><br>";
-		// echo "new : ".$dataOK;
+		// echo "data is :   ".$data."<br><br>";
+		// echo "new : ".$dataOK->Username;
+
+
 
 		$userId = $dataOK->Username;
 		$pass = $dataOK->Password;
 
-		if($userId == $_POST['Username'] && $pass == $_POST['Password'])
+
+
+		// echo "id : ".$dataOK->Username;
+		// echo "pass : ".$dataOK->Password;
+
+		if($userId === $_POST['Username'] and $pass === $_POST['Password'])
 		{
+			// echo "id is: ".$userId."<br>";
+			// echo "id1 is: ".$_POST['Username']."<br>";
+			// echo "pa is: ".$pass."<br>";
+			// echo "id is: ".$_POST['Password']."<br>";
 			header('Location: welcome.html');
+
 			//exit;
 		}
 		else
 		{
 			$failed = "Log-in failed";
+
+
 		}
+	}
 
 		// foreach ($dtlsOK as $ok  )
 		// {
@@ -53,15 +75,15 @@
 	?>
 	
 	<form action="<?php echo htmlspecialchars(($_SERVER['PHP_SELF'])); ?>" method = "POST">
- 	 
- 		 
-			<label for="Username">Username:</label>
-			<input type="text" id="Username" name="Username" required><br>
+		
+		
+		<label for="Username">Username:</label>
+		<input type="text" id="Username" name="Username" required><br>
 
-			<label for="Password">Password:</label>
-			<input type="Password" id="Password" name="Password" required><br>
+		<label for="Password">Password:</label>
+		<input type="Password" id="Password" name="Password" required><br>
 
- 		<br>
+		<br>
 		<input type="submit" value="Log-in">
 		<span style="color: red"><?php echo $failed; ?></span>
 
